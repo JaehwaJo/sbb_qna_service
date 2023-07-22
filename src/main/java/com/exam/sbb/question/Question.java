85ac95b0cf8ecde323e173b962e8cfd41e307586
jaehwaJo <woghk2233@naver.com>
2023-07-22 14:34:20 +0900

    Question 엔티티에 OneToMany 관계로 Answer List 필드 추가

--- src/main/java/com/exam/sbb/question/Question.java
+++ src/main/java/com/exam/sbb/question/Question.java
@@ -1,9 +1,11 @@
 package com.exam.sbb.question;
 
+import com.exam.sbb.answer.Answer;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
@@ -19,4 +21,7 @@ public class Question {
   private String content;
 
   private LocalDateTime createDate;
+
+  @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+  private List<Answer> answerList;
 }
