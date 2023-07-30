939b8ed9366526d239fe4b43da0e326a6d5aed8d
jaehwaJo <woghk2233@naver.com>
2023-07-30 14:32:18 +0900

    Question 엔티티 추가

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -1,212 +1,254 @@
 package com.exam.sbb.controller;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class MainController {
-//  @RequestMapping("/sbb")
-//  // 아래 함수의 리턴값을 그대로 브라우저에 표시
-//  // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
-//  @ResponseBody
-//  public String index() {
-//    return "안녕하세요.";
-//  }
-//
-//  @GetMapping("/page1")
-//  @ResponseBody
-//  public String showGet() {
-//    return """
-//           <form method="POST" action="/page2" />
-//              <input type="number" name="age" placeholder="나이 입력" />
-//              <input type="submit" value="page2로 POST 방식으로 이동" />
-//           </form>
-//           """;
-//  }
-//
-//
-//  @PostMapping("/page2")
-//  @ResponseBody
-//  public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
-//    return """
-//           <h1>입력된 나이 : %d</h1>
-//           <h1>안녕하세요. POST 방식으로 오신걸 환영합니다.</h1>
-//           """.formatted(age);
-//  }
-//
-//  @GetMapping("/page2")
-//  @ResponseBody
-//  public String showPost(@RequestParam(defaultValue = "0") int age) {
-//    return """
-//           <h1>입력된 나이 : %d</h1>
-//           <h1>안녕하세요. GET 방식으로 오신걸 환영합니다.</h1>
-//           """.formatted(age);
-//  }
-//  @GetMapping("/plus")
-//  @ResponseBody
-//  public String plusNum(int a, int b) {
-//    return """
-//           <h1>%d</h1>
-//           """.formatted(a + b);
-//  }
-//
-//  @GetMapping("/minus")
-//  @ResponseBody
-//  public String minusNum(int a, int b) {
-//    return """
-//           <h1>%d</h1>
-//           """.formatted(a - b);
-//  }
-//  private int a = 0;
-//  @GetMapping("/increase")
-//  @ResponseBody
-//  public int increaseNum() {
-//
-//    return a++;
-//  }
-//
-//  @GetMapping("/gugudan")
-//  @ResponseBody
-//  public String gugudan(Integer dan, Integer limit) {
-//    if (dan == null) {
-//      dan = 9;
-//    }
-//    if (limit == null) {
-//      limit = 9;
-//    }
-//
-//    final Integer finaldan = dan;
-//    return IntStream.rangeClosed(1, limit)
-//        .mapToObj(i -> "%d * %d = %d".formatted(finaldan, i, finaldan * i))
-//        .collect(Collectors.joining("<br>"));
-//  }
-//
-//  @GetMapping("/mbti")
-//  @ResponseBody
-//  public String whatMbti(String name) {
-//    String mbti = "";
-//
-//    switch (name) {
-//      case "홍길동" :
-//        mbti = "INFP";
-//        break;
-//      case "홍길순" :
-//        mbti = "ENFP";
-//        break;
-//      case "임꺽정" :
-//        mbti = "ESFJ";
-//        break;
-//    }
-//
-//    return mbti;
-//  }
-//
-//  @GetMapping("/saveSessionAge/{age}")
-//  @ResponseBody
-//  public String saveAge(HttpServletRequest request,@PathVariable int age) {
-//    HttpSession session = request.getSession();
-//
-//    session.setAttribute("age", age);
-//
-//    return "Session에 %d저장.".formatted(age);
-//  }
-//
-//  @GetMapping("/getSessionAge")
-//  @ResponseBody
-//  public String getAge(HttpServletRequest request) {
-//    HttpSession session = request.getSession();
-//
-//    return "Session에 저장된 값 : %d".formatted((int) session.getAttribute("age"));
-//  }
-  @AllArgsConstructor
-  @Getter
-  @Setter
-  class Article {
-    static int lastId = 0;
-    int id;
-    String title;
-    String body;
-    Article(String title, String body) {
-      this(++lastId, title, body);
+
+  private int increaseNo = -1;
+
+  @RequestMapping("/sbb")
+  // 아래 함수의 리턴값을 그대로 브라우저에 표시
+  // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
+  @ResponseBody
+  public String index() {
+    return "안녕하세요.asdddzzzz";
+  }
+
+  @GetMapping("/page1")
+  @ResponseBody
+  public String showGet() {
+    return """
+        <form method="POST" action="/page2" />
+           <input type="number" name="age" placeholder="나이 입력" />
+           <input type="submit" value="page2로 POST 방식으로 이동" />
+        </form>
+        """;
+  }
+
+
+  @PostMapping("/page2")
+  @ResponseBody
+  public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+    return """
+        <h1>입력된 나이 : %d</h1>
+        <h1>안녕하세요. POST 방식으로 오신걸 환영합니다.</h1>
+        """.formatted(age);
+  }
+
+  @GetMapping("/page2")
+  @ResponseBody
+  public String showPost(@RequestParam(defaultValue = "0") int age) {
+    return """
+        <h1>입력된 나이 : %d</h1>
+        <h1>안녕하세요. GET 방식으로 오신걸 환영합니다.</h1>           
+        """.formatted(age);
+  }
+
+  @GetMapping("/plus")
+  @ResponseBody
+  public int showPlus(int a, int b) {
+    return a + b;
+  }
+
+  @GetMapping("/plus2")
+  @ResponseBody
+  public void showPlus2(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    int a = Integer.parseInt(req.getParameter("a"));
+    int b = Integer.parseInt(req.getParameter("b"));
+
+    resp.getWriter().append(a + b + "");
+  }
+
+  @GetMapping("/minus")
+  @ResponseBody
+  public int showMinus(int a, int b) {
+    return a - b;
+  }
+
+  @GetMapping("/increase")
+  @ResponseBody
+  public int showIncrease() {
+    increaseNo++;
+    return increaseNo;
+  }
+
+  @GetMapping("/gugudan")
+  @ResponseBody
+  public String showGugudan(Integer dan, Integer limit) {
+    if (dan == null) {
+      dan = 9;
     }
-}
-  List<Article> articles = new ArrayList<>();
+
+    if (limit == null) {
+      limit = 9;
+    }
+
+    final Integer finalDan = dan;
+    return IntStream.rangeClosed(1, limit)
+        .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
+        .collect(Collectors.joining("<br>\n"));
+  }
+
+  @GetMapping("/mbti/{name}")
+  @ResponseBody
+  public String showMbti(@PathVariable String name) {
+    return switch (name) {
+      case "홍길순" -> {
+        char j = 'J';
+        yield "INF" + j;
+      }
+      case "임꺽정" -> "ESFJ";
+      case "홍길동", "박상원" -> "INFP";
+      default -> "모름";
+    };
+  }
+
+
+  @GetMapping("/saveSession/{name}/{value}")
+  @ResponseBody
+  public String saveSession(@PathVariable String name, @PathVariable String value, HttpServletRequest req) {
+    HttpSession session = req.getSession();
+    session.setAttribute(name, value);
+
+    return "세션변수 %s의 값이 %s(으)로 설정되었습니다.".formatted(name, value);
+  }
+
+  @GetMapping("/getSession/{name}")
+  @ResponseBody
+  public String getSession(@PathVariable String name, HttpSession session) {
+    // req => 쿠키 => JSESSIONID => 세션을 얻을 수 있다.
+
+    String value = (String) session.getAttribute(name);
+
+    return "세션변수 %s의 값은 %s입니다.".formatted(name, value);
+  }
+
+  private List<Article> articles = new ArrayList<>(
+      Arrays.asList(
+          new Article("제목", "내용"),
+          new Article("제목", "내용")
+      )
+  );
 
   @GetMapping("/addArticle")
   @ResponseBody
   public String addArticle(String title, String body) {
 
     Article article = new Article(title, body);
-
     articles.add(article);
 
-    return "%d번 글이 등록되었습니다.".formatted(article.getId());
+    return "%d번 게시물이 생성되었습니다.".formatted(article.getId());
   }
 
   @GetMapping("/article/{id}")
   @ResponseBody
-  public Article getAticle(@PathVariable int id) {
-    Article article = articles.stream().filter(a -> a.getId() == id).findFirst().get();
+  public Article getArticle(@PathVariable int id) {
+
+    Article article = articles // id가 1번인 게시물이 앞에서 3번째
+        .stream()
+        .filter(a -> a.getId() == id)
+        .findFirst()
+        .orElse(null);
 
     return article;
   }
-  @GetMapping("/modifyArticle")
+
+  @GetMapping("/modifyArticle/{id}")
   @ResponseBody
-  public String modifyArticle(int id, String title, String body) {
-    Article article = articles.stream().filter(a -> a.getId() == id).findFirst().get();
-    article.title = title;
-    article.body = body;
+  public String modifyArticle(@PathVariable int id, String title, String body) {
 
-    articles.set(id - 1, article);
+    Article article = articles // id가 1번인 게시물이 앞에서 3번째
+        .stream()
+        .filter(a -> a.getId() == id)
+        .findFirst()
+        .orElse(null);
 
-    return "%d번 글이 수정되었습니다..".formatted(article.getId());
+    if (article == null) {
+      return "%d번 게시물은 존재하지 않습니다.".formatted(id);
+    }
+
+    article.setTitle(title);
+    article.setBody(body);
+
+    return "%d번 게시물을 수정하였습니다.".formatted(article.getId());
   }
+
   @GetMapping("/deleteArticle/{id}")
   @ResponseBody
   public String deleteArticle(@PathVariable int id) {
-    Article article = articles.stream().filter(a -> a.getId() == id).findFirst().get();
 
-    articles.remove(id - 1);
+    Article article = articles
+        .stream()
+        .filter(a -> a.getId() == id)
+        .findFirst()
+        .orElse(null);
 
-    return "%d번 글이 삭제되었습니다..".formatted(article.getId());
-  }
-  @AllArgsConstructor
-  @NoArgsConstructor
-  @Getter
-  @Setter
-  class Person {
-    private int id;
-    private int age;
-    private String name;
-
-    public Person(int age, String name) {
-      this.age = age;
-      this.name = name;
+    if (article == null) {
+      return "%d번 게시물은 존재하지 않습니다.".formatted(id);
     }
+
+    articles.remove(article);
+
+    return "%d번 게시물을 삭제하였습니다.".formatted(article.getId());
   }
 
-//  @GetMapping("/addPerson")
-//  @ResponseBody
-//  public String addPerson(int id, int age, String name) {
-//    Person p = new Person(id, age, name);
-//
-//    return "id : %d, age : %d, name : %s입니다.".formatted(p.getId(), p.getAge(), p.getName());
-//  }
+  @GetMapping("/addPersonOnlyWay")
+  @ResponseBody
+  public Person addPersonOnlyWay(int id, int age, String name) {
+    Person p = new Person(id, age, name);
+    return p;
+  }
 
+  // 액션 메서드드
   @GetMapping("/addPerson/{id}")
   @ResponseBody
-  public String addPerson(@PathVariable Person p) {
-    //p = new Person(p.id, age, name);
+  public Person addPerson(Person p) {
+    return p;
+  }
+}
 
-    return "id : %d, age : %d, name : %s입니다.".formatted(p.getId(), p.getAge(), p.getName());
+@AllArgsConstructor
+@Getter
+@Setter
+class Article {
+  private static int lastId = 0;
+  private int id;
+  private String title;
+  private String body;
+
+  public Article(String title, String body) {
+    this(++lastId, title, body);
+  }
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Person {
+  private int id;
+  private int age;
+  private String name;
+
+  public Person(int age, String name) {
+    this.age = age;
+    this.name = name;
   }
 }
