625ca9a50794703fc6937e72e81df0d084ff9700
jaehwaJo <woghk2233@naver.com>
2023-08-26 13:33:50 +0900

    질문 작성시에 현재 로그인한 회원을 작성자로 지정

--- src/main/java/com/exam/sbb/question/QuestionController.java
+++ src/main/java/com/exam/sbb/question/QuestionController.java
@@ -1,6 +1,9 @@
 package com.exam.sbb.question;
 
 import com.exam.sbb.answer.AnswerForm;
+import com.exam.sbb.user.SiteUser;
+import com.exam.sbb.user.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequestMapping("/question")
 @Controller
@@ -18,14 +23,17 @@ import org.springframework.web.bind.annotation.*;
 // 서비스는 컨트롤러를 몰라야 한다.
 // DB는 리포지터리를 몰라야 한다.
 // SPRING DATA JPA는 MySQL을 몰라야 한다.
- // SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
+// SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
   // @Autowired 필드 주입
   private final QuestionService questionService;
 
+  private final UserService userService;
+
   @GetMapping("/list")
   // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html 파일을 뷰로 삼는다.
-  public String list(Model model, @RequestParam(defaultValue="0") int page) {
+  public String list(HttpSession session, Model model, @RequestParam(defaultValue="0") int page) {
+
     Page<Question> paging = questionService.getList(page);
     // 미리 실행된 question_list.html에서
     // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
@@ -47,13 +55,15 @@ public class QuestionController {
   }
 
   @PostMapping("/create")
-  public String questionCreate(Model model, @Valid QuestionForm questionForm, BindingResult bindingResult) {
+  public String questionCreate(Principal principal, Model model, @Valid QuestionForm questionForm, BindingResult bindingResult) {
+    SiteUser siteUser = userService.getUser(principal.getName());
+
 
     if(bindingResult.hasErrors()) {
       return "question_form";
     }
 
-    questionService.create(questionForm.getSubject(), questionForm.getContent());
+    questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
     return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
   }
 }
