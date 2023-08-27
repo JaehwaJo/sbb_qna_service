47a5e5d1765d1a9f4e9199086318071175666972
jaehwaJo <woghk2233@naver.com>
2023-08-27 14:18:53 +0900

    답변 삭제 처리

--- src/main/java/com/exam/sbb/answer/AnswerService.java
+++ src/main/java/com/exam/sbb/answer/AnswerService.java
@@ -27,4 +27,13 @@ public class AnswerService {
     return answerRepository.findById(id).orElseThrow(() -> new DataNotFoundException("answer not found"));
   }
 
+  public void modify(Answer answer, String content) {
+    answer.setContent(content);
+    answer.setModifyDate(LocalDateTime.now());
+    answerRepository.save(answer);
+  }
+
+  public void delete(Answer answer) {
+    answerRepository.delete(answer);
+  }
 }
