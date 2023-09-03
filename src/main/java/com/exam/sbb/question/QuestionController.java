d8d012797502d1fe30fa145e7db3600649c4c342
jaehwaJo <woghk2233@naver.com>
2023-09-03 12:59:52 +0900

    kw 파라미터가 질문제목 검색어로 작동

--- src/main/java/com/exam/sbb/question/QuestionController.java
+++ src/main/java/com/exam/sbb/question/QuestionController.java
@@ -3,7 +3,6 @@ package com.exam.sbb.question;
 import com.exam.sbb.answer.AnswerForm;
 import com.exam.sbb.user.SiteUser;
 import com.exam.sbb.user.UserService;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -26,7 +25,7 @@ import java.security.Principal;
 // 서비스는 컨트롤러를 몰라야 한다.
 // DB는 리포지터리를 몰라야 한다.
 // SPRING DATA JPA는 MySQL을 몰라야 한다.
- // SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
+// SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
   // @Autowired 필드 주입
   private final QuestionService questionService;
@@ -35,9 +34,9 @@ public class QuestionController {
 
   @GetMapping("/list")
   // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html 파일을 뷰로 삼는다.
-  public String list(HttpSession session, Model model, @RequestParam(defaultValue="0") int page) {
+  public String list(String kw, Model model, @RequestParam(defaultValue="0") int page) {
 
-    Page<Question> paging = questionService.getList(page);
+    Page<Question> paging = questionService.getList(kw, page);
     // 미리 실행된 question_list.html에서
     // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
     model.addAttribute("paging", paging);
